cfb242e13a4edd5688bd1eefc12eb58571c7c59c
Ivan Rodriguez Gonzalez <ivan.rodriguez.gonzalez@gmail.com>
2018-04-13 22:14:04 +0200

    cambiado el directorio de ficheros raml al directorio por defecto del proyecto

--- InventarioDeAPI/src/Principal.java
+++ InventarioDeAPI/src/Principal.java
@@ -1,5 +1,5 @@
 public class Principal {
-    private final String PATH = "C:\\Users\\u8332\\Desktop\\Raml\\";
+    private final String PATH = ".//";
 
     public static void main(String[] args) {
         new Principal().ejecutar();
